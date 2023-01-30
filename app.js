b78a42542e6aadabdea79de55b7b7b5c9dc02b36
hshrivastav <himanshushrivastava2881@gmail.com>
2023-01-30 23:14:21 +0530

    update vercel config

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-import dotenv from "dotenv";
+const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const bodyParser = require("body-parser");
